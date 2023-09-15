b6880923040a9e8a778c834e6d017af0c28f27bd
ThomasJohnen <thomas.johnen@hotmail.be>
2023-09-15 14:46:25 +0100

    1.1-1.2 : refactor - modules

--- exercices/1.1-1.2/part1/src/App.jsx
+++ exercices/1.1-1.2/part1/src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "./header"
-import Content from "./content"
-import Total from "./Total.jsx"
+import Header from "./components/Header/Header"
+import Content from "./components/Content/Content"
+import Total from "./components/Total/Total.jsx"
 
 const App = () => {
   const course = 'Half Stack application development'
